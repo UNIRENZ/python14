65e5b1ea95134adb98f94ab43b3bb8cdaf830c98
UNIRENZ <108718227+UNIRENZ@users.noreply.github.com>
2022-07-26 14:30:41 +0700

    Add files via upload

---
+++ work-function.py
@@ -0,0 +1,11 @@
+from cmath import pi
+
+
+def square(width,height):
+    return width * height
+def triple(base, height):
+    return 0.5 * base * height
+def circle(radius):
+    return pi*radius*radius
+    
+#นายเนติลักษณ์ ดาราเรือง เลขที่1 ม.6/14
